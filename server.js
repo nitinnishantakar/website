29223d89e421f2aee25b8e1eeb152c5080b6098a
= <nitinnishantakar@gmail.com>
2021-11-03 00:42:08 +0530

    new

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express')
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.get("/", (request, response) => {
 	response.send("this is nitin");
 })
 
-app.listen('3000', () => {
+app.listen(port, () => {
 	console.log("server started at port 3000")
 })
