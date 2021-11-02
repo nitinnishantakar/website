1624d84ec5cd5af57ec0be73d402c22e5a843cb8
= <nitinnishantakar@gmail.com>
2021-11-02 20:40:13 +0530

    inital commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var app = express();
+
+app.get("/", (request, response) => {
+	request.send("this is nitin");
+})
+
+app.listen('3000', () => {
+	console.log("server started at port 3000")
+})
